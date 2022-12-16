aa0702ff852a59b2a5b9b66552637af655fd21c7
Tamara Eaton <Tamara.Eaton@bounteous.com>
2022-12-16 14:07:41 -0700

    changed basename

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="challenge-after">
+    <BrowserRouter basename="challengeafter">
       <App />
     </BrowserRouter>
   </React.StrictMode>
